1b136a52f9534ae5700bc81fc291b8b4725c2df5
Dhiren Bhatia <dhirenb@gmail.com>
2012-03-04 18:32:59 -0800

    User has many posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,9 @@
 class Post < ActiveRecord::Base
+  attr_accessible :content
+
+  belongs_to :user
+
+  validates :content, :presence => true, :length => { :maximum => 140 }
+  validates :user_id, :presence => true
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
 
   validates_uniqueness_of :email
+
+  has_many :posts, :dependent => :destroy
 end
