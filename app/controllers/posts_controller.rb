c9a061d9d7dd82ee7dddd3cb8cb6830cb2f9e27f
Dhiren Bhatia <dhirenb@gmail.com>
2012-03-04 18:59:22 -0800

    Stylistic changes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,13 @@
 class PostsController < ApplicationController
+  before_filter :authenticate_user!
+
   def create
+    @post = current_user.posts.build(params[:post])
+    if @post.save
+      redirect_to root_path
+    else
+      render 'main/home'
+    end
   end
 
   def destroy
