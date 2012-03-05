c994aea09fe9ea941be0536f96682556782b6387
Dhiren Bhatia <dhirenb@gmail.com>
2012-03-04 17:50:38 -0800

    Create initial rails project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwitterClone::Application.config.secret_token = '5c67e9b8844a0c3ea41773871cf843bad3b53bbacbedd4a5b1e523791dc2c49b3e044ca92c9b1691c8e8231570797ccaf285997c945abad3d612e75b1a566e52'
